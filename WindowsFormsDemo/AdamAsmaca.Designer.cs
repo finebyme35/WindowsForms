15106ad272d5006a1fcf0b57bab18dfb32bfa16e
oguzcan akyol <craboguz35@hotmail.com>
2021-02-23 16:30:42 +0300

    AdamAsmacaAdded

--- WindowsFormsDemo/AdamAsmaca.Designer.cs
+++ WindowsFormsDemo/AdamAsmaca.Designer.cs
@@ -35,12 +35,15 @@ namespace WindowsFormsDemo
             this.lbSonuc = new System.Windows.Forms.ListBox();
             this.bGöster = new System.Windows.Forms.Button();
             this.lDebugGelenKelime = new System.Windows.Forms.Label();
+            this.pbAdam = new System.Windows.Forms.PictureBox();
+            this.lBilgi = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAdam)).BeginInit();
             this.SuspendLayout();
             // 
             // lRasgele
             // 
             this.lRasgele.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lRasgele.Location = new System.Drawing.Point(177, 21);
+            this.lRasgele.Location = new System.Drawing.Point(183, 33);
             this.lRasgele.Name = "lRasgele";
             this.lRasgele.Size = new System.Drawing.Size(169, 38);
             this.lRasgele.TabIndex = 0;
@@ -49,7 +52,7 @@ namespace WindowsFormsDemo
             // lGiris
             // 
             this.lGiris.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lGiris.Location = new System.Drawing.Point(12, 93);
+            this.lGiris.Location = new System.Drawing.Point(-1, 119);
             this.lGiris.Name = "lGiris";
             this.lGiris.Size = new System.Drawing.Size(159, 34);
             this.lGiris.TabIndex = 1;
@@ -57,7 +60,7 @@ namespace WindowsFormsDemo
             // 
             // tbGiris
             // 
-            this.tbGiris.Location = new System.Drawing.Point(145, 93);
+            this.tbGiris.Location = new System.Drawing.Point(132, 119);
             this.tbGiris.Name = "tbGiris";
             this.tbGiris.Size = new System.Drawing.Size(245, 20);
             this.tbGiris.TabIndex = 2;
@@ -65,7 +68,7 @@ namespace WindowsFormsDemo
             // lbSonuc
             // 
             this.lbSonuc.FormattingEnabled = true;
-            this.lbSonuc.Location = new System.Drawing.Point(427, 33);
+            this.lbSonuc.Location = new System.Drawing.Point(405, 12);
             this.lbSonuc.Name = "lbSonuc";
             this.lbSonuc.Size = new System.Drawing.Size(146, 173);
             this.lbSonuc.TabIndex = 11;
@@ -73,7 +76,7 @@ namespace WindowsFormsDemo
             // bGöster
             // 
             this.bGöster.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.bGöster.Location = new System.Drawing.Point(226, 133);
+            this.bGöster.Location = new System.Drawing.Point(213, 159);
             this.bGöster.Name = "bGöster";
             this.bGöster.Size = new System.Drawing.Size(75, 23);
             this.bGöster.TabIndex = 12;
@@ -84,17 +87,38 @@ namespace WindowsFormsDemo
             // lDebugGelenKelime
             // 
             this.lDebugGelenKelime.AutoSize = true;
-            this.lDebugGelenKelime.Location = new System.Drawing.Point(163, 219);
+            this.lDebugGelenKelime.Location = new System.Drawing.Point(129, 220);
             this.lDebugGelenKelime.Name = "lDebugGelenKelime";
             this.lDebugGelenKelime.Size = new System.Drawing.Size(38, 13);
             this.lDebugGelenKelime.TabIndex = 13;
             this.lDebugGelenKelime.Text = "Sonuc";
+            // 
+            // pbAdam
+            // 
+            this.pbAdam.Location = new System.Drawing.Point(583, 16);
+            this.pbAdam.Name = "pbAdam";
+            this.pbAdam.Size = new System.Drawing.Size(205, 166);
+            this.pbAdam.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbAdam.TabIndex = 14;
+            this.pbAdam.TabStop = false;
+            // 
+            // lBilgi
+            // 
+            this.lBilgi.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lBilgi.Location = new System.Drawing.Point(115, 66);
+            this.lBilgi.Name = "lBilgi";
+            this.lBilgi.Size = new System.Drawing.Size(262, 40);
+            this.lBilgi.TabIndex = 15;
+            this.lBilgi.Text = "lBilgi";
+            this.lBilgi.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // AdamAsmaca
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lBilgi);
+            this.Controls.Add(this.pbAdam);
             this.Controls.Add(this.lDebugGelenKelime);
             this.Controls.Add(this.bGöster);
             this.Controls.Add(this.lbSonuc);
@@ -104,6 +128,7 @@ namespace WindowsFormsDemo
             this.Name = "AdamAsmaca";
             this.Text = "AdamAsmaca";
             this.Load += new System.EventHandler(this.AdamAsmaca_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbAdam)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -117,5 +142,7 @@ namespace WindowsFormsDemo
         private System.Windows.Forms.ListBox lbSonuc;
         private System.Windows.Forms.Button bGöster;
         private System.Windows.Forms.Label lDebugGelenKelime;
+        private System.Windows.Forms.PictureBox pbAdam;
+        private System.Windows.Forms.Label lBilgi;
     }
 }
--- WindowsFormsDemo/AdamAsmaca.cs
+++ WindowsFormsDemo/AdamAsmaca.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,55 +16,123 @@ namespace WindowsFormsDemo
     {
         public string[] Kelime = new string[]
         {
-            "Adam",
-            "Salı",
-            "Silme",
-            "Çember",
-            "Çevirme",
-            "Kolkoparan"
+            "İSTANBUL",
+            "İZMİR",
+            "ANKARA",
+            "LOSANGELES",
+            "ABUDHABİ",
+            "ÇANAKKALE"
         };
-        
+
+        public string buluncakKelime = "";
+        public int resimSayisi = 0;
+
+
         public Random rand = new Random();
-        
+
         public AdamAsmaca()
         {
             InitializeComponent();
         }
+
         private void AdamAsmaca_Load(object sender, EventArgs e)
         {
             lRasgele.Text = "";
-           
+            lBilgi.Text = "BU BİR ŞEHİR";
+            lDebugGelenKelime.Text = "";
+            buluncakKelime = Kelime[rand.Next(6)].ToUpper();
+            for (int i = 0; i < buluncakKelime.Length; i++)
+            {
+
+                lRasgele.Text += "*";
+            }
+
+            resimSayisi = 0;
+            pbAdam.Image = null;
+            lDebugGelenKelime.Text = buluncakKelime;
+            lbSonuc.Items.Clear();
+            lDebugGelenKelime.Visible = false;
+
+
 
         }
 
         private void bGöster_Click(object sender, EventArgs e)
         {
-           
-            for (int i = 0; i <= Kelime.Length - 1; i++)
-            {
-                lbSonuc.Items.Add(Kelime[i].ToLower());
-            }
-
-            int a = rand.Next(Kelime.Length);
-            string gelenKelime = Kelime[a];
-            int j = 0;
-            string isaret = "_";
-
-            while (j <= gelenKelime.Length )
-            {
-                
-                lRasgele.Text = Convert.ToString(j);
-                lDebugGelenKelime.Text = gelenKelime;
-                j++;
-            }
-
-          
-
-            
-            
+            islemler();
 
         }
 
-        
+        void islemler()
+        {
+            if (string.IsNullOrWhiteSpace(tbGiris.Text) || tbGiris.Text.Length > 1)
+            {
+                MessageBox.Show("Lütfen 1 Harf Giriniz.");
+                tbGiris.Text = "";
+            }
+            else
+            {
+                bool result;
+
+                char aranacakChar = char.Parse(tbGiris.Text.ToUpper());
+                result = char.IsLetter(aranacakChar);
+                if (result == true)
+                {
+                    char[] karakterler = buluncakKelime.ToCharArray();
+                    bool varmi = false;
+
+                    for (int i = 0; i <= lbSonuc.Items.Count - 1; i++)
+                    {
+                        if (lbSonuc.Items[i].ToString() == aranacakChar.ToString())
+                        {
+                            MessageBox.Show("Bu harf daha önce girilmiştir. Başka Harf deneyin.");
+                            return;
+                        }
+                    }
+
+                    lbSonuc.Items.Add(aranacakChar.ToString());
+
+                    for (int i = 0; i < karakterler.Length; i++)
+                    {
+                        if (karakterler[i] == aranacakChar)
+                        {
+                            lRasgele.Text = lRasgele.Text.Remove(i, 1);
+                            lRasgele.Text = lRasgele.Text.Insert(i, aranacakChar.ToString());
+                            varmi = true;
+                        }
+                    }
+
+                    if (lRasgele.Text == buluncakKelime)
+                    {
+                        MessageBox.Show("Kelime'yi bildiniz. TEBRİKLER.");
+                        lRasgele.Text = buluncakKelime;
+                        return;
+                    }
+
+                    tbGiris.Text = "";
+
+                    if (varmi == false)
+                    {
+                        resimSayisi++;
+                        pbAdam.Image = Image.FromFile(@"D:\EğitimBilgeAdam\WindowsFormsExample\WindowsFormsDemo\WindowsFormsDemo\Resimler\" + resimSayisi + ".png");
+
+                        if (resimSayisi == 7)
+                        {
+                            MessageBox.Show("Bütün Haklarınız doldu oyunu kaybettiniz.");
+                            lRasgele.Text = buluncakKelime;
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen Harf Giriniz.");
+                }
+            }
+        }
+
+
     }
 }
+
+
